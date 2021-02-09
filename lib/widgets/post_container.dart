3fcd1c93d7f6efb61af56401da8eff8b02687099
IgorGarciaCosta <igonildo7@gmail.com>
2021-02-09 13:37:01 -0300

    :art: Continuing post container
    Adding to post the prifile image, name and time, and adding the respective image.

--- lib/widgets/post_container.dart
+++ lib/widgets/post_container.dart
@@ -1,8 +1,10 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/models/models.dart';
+import 'package:myapp/widgets/widgets.dart';
 
 class PostContainer extends StatelessWidget {
-final Post post;
+  final Post post;
 
   const PostContainer({Key key, @required this.post}) : super(key: key);
 
@@ -10,9 +12,76 @@ final Post post;
   Widget build(BuildContext context) {
     return Container(
       child: Container(
-        height: 100,
-        color: Colors.red,
+        margin: const EdgeInsets.symmetric(vertical: 5),
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        color: Colors.white,
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  _PostHeader(post: post),
+                  const SizedBox(height: 4),
+                  Text(post.caption),
+                  post.imageUrl != null
+                      ? const SizedBox.shrink()
+                      : const SizedBox(height: 6),
+                ],
+              ),
+            ),
+            post.imageUrl != null
+                ? Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8),
+                    child: CachedNetworkImage(imageUrl: post.imageUrl))
+                : const SizedBox.shrink(),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class _PostHeader extends StatelessWidget {
+  final Post post;
+
+  const _PostHeader({Key key, @required this.post}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        ProfileAvatar(imageUrl: post.user.imageUrl),
+        const SizedBox(width: 8),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                post.user.name,
+                style: TextStyle(fontWeight: FontWeight.w600),
+              ),
+              Row(
+                children: <Widget>[
+                  Text(
+                    '${post.timeAgo} • ',
+                    style: TextStyle(
+                      color: Colors.grey[600],
+                      fontSize: 12,
+                    ),
+                  ),
+                  Icon(Icons.public, color: Colors.grey[600], size: 12),
+                  IconButton(
+                    icon: const Icon(Icons.more_horiz),
+                    onPressed: () => print('More'),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )
+      ],
     );
   }
 }
