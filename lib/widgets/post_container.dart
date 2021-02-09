5c67d195471a79eb0f81e1869d962b69f3b5157e
IgorGarciaCosta <igonildo7@gmail.com>
2021-02-09 13:58:13 -0300

    :art: Finalizing post container
    Adding like icon, likes and shares numbers and the buttons for Like, share and comment below.

--- lib/widgets/post_container.dart
+++ lib/widgets/post_container.dart
@@ -1,5 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:myapp/config/pallete.dart';
 import 'package:myapp/models/models.dart';
 import 'package:myapp/widgets/widgets.dart';
 
@@ -36,6 +38,10 @@ class PostContainer extends StatelessWidget {
                     padding: const EdgeInsets.symmetric(vertical: 8),
                     child: CachedNetworkImage(imageUrl: post.imageUrl))
                 : const SizedBox.shrink(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              child: _PostStats(post: post),
+            )
           ],
         ),
       ),
@@ -82,6 +88,109 @@ class _PostHeader extends StatelessWidget {
           ),
         )
       ],
+    );
+  }
+}
+
+class _PostStats extends StatelessWidget {
+  final Post post;
+  const _PostStats({Key key, @required this.post}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Container(
+              padding: const EdgeInsets.all(4),
+              decoration: BoxDecoration(
+                  color: Palette.facebookBlue, shape: BoxShape.circle),
+              child: const Icon(
+                Icons.thumb_up,
+                size: 10,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(width: 4),
+            Expanded(
+              child: Text(
+                '${post.likes}',
+                style: TextStyle(color: Colors.grey[600]),
+              ),
+            ),
+            Text(
+              '${post.comments} Comments',
+              style: TextStyle(color: Colors.grey[600]),
+            ),
+            SizedBox(width: 8),
+            Text(
+              '${post.shares} Shares',
+              style: TextStyle(color: Colors.grey[600]),
+            ),
+          ],
+        ),
+        const Divider(),
+        Row(
+          children: <Widget>[
+            _PostButton(
+              icon: Icon(MdiIcons.thumbUpOutline,
+                  color: Colors.grey[600], size: 20),
+              label: 'Like',
+              onTap: () => print('Like'),
+            ),
+            _PostButton(
+              icon: Icon(MdiIcons.commentOutline,
+                  color: Colors.grey[600], size: 20),
+              label: 'Comment',
+              onTap: () => print('Comment'),
+            ),
+            _PostButton(
+              icon: Icon(MdiIcons.shareOutline,
+                  color: Colors.grey[600], size: 25),
+              label: 'Share',
+              onTap: () => print('Share'),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class _PostButton extends StatelessWidget {
+  final Icon icon;
+  final String label;
+  final Function onTap;
+  const _PostButton(
+      {Key key,
+      @required this.icon,
+      @required this.label,
+      @required this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Material(
+        //Wrapping the inkwell inside a Material allow me to use the Ink effect
+        color: Colors.white,
+        child: InkWell(
+          onTap: onTap,
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            height: 25,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                icon,
+                SizedBox(width: 4),
+                Text(label),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
